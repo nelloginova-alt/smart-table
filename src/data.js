9f69a42897308771456a9901e1f9792bb1e831ce
Nelly Lisovik <nel.loginova@gmail.com>
2026-02-27 10:45:42 +0300

    проверка

--- src/data.js
+++ src/data.js
@@ -4,34 +4,34 @@ const BASE_URL = 'https://webinars.webdev.education-services.ru/sp7-api';
 
 export function initData(sourceData) {
     // переменные для кеширования данных
-let sellers;
-let customers;
-let lastResult;
-let lastQuery;
+    let sellers;
+    let customers;
+    let lastResult;
+    let lastQuery;
 
 // функция для приведения строк в тот вид, который нужен нашей таблице
-const mapRecords = (data) => data.map(item => ({
-    id: item.receipt_id,
-    date: item.date,
-    seller: sellers[item.seller_id],
-    customer: customers[item.customer_id],
-    total: item.total_amount
-}));
+    const mapRecords = (data) => data.map(item => ({
+        id: item.receipt_id,
+        date: item.date,
+        seller: sellers[item.seller_id],
+        customer: customers[item.customer_id],
+        total: item.total_amount
+    }));
 
 // функция получения индексов
-const getIndexes = async () => {
-    if (!sellers || !customers) { // если индексы ещё не установлены, то делаем запросы
-        [sellers, customers] = await Promise.all([ // запрашиваем и деструктурируем в уже объявленные ранее переменные
-            fetch(`${BASE_URL}/sellers`).then(res => res.json()), // запрашиваем продавцов
-            fetch(`${BASE_URL}/customers`).then(res => res.json()), // запрашиваем покупателей
-        ]);
+    const getIndexes = async () => {
+        if (!sellers || !customers) { // если индексы ещё не установлены, то делаем запросы
+            [sellers, customers] = await Promise.all([ // запрашиваем и деструктурируем в уже объявленные ранее переменные
+                fetch(`${BASE_URL}/sellers`).then(res => res.json()), // запрашиваем продавцов
+                fetch(`${BASE_URL}/customers`).then(res => res.json()), // запрашиваем покупателей
+            ]);
+        }
+
+        return { sellers, customers };
     }
 
-    return { sellers, customers };
-}
-
 // функция получения записей о продажах с сервера
-const getRecords = async (query, isUpdated = false) => {
+    const getRecords = async (query, isUpdated = false) => {
         const qs = new URLSearchParams(query); // преобразуем объект параметров в SearchParams объект, представляющий query часть url
         const nextQuery = qs.toString(); // и приводим к строковому виду
 
@@ -52,8 +52,8 @@ const getRecords = async (query, isUpdated = false) => {
         return lastResult;
     };
 
-return {
-    getIndexes,
-    getRecords
-};
+    return {
+        getIndexes,
+        getRecords
+    };
 }
